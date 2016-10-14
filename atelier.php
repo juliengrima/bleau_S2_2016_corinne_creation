c7453fbc2a5f7c928a58a62edadb38e7b5f83892
juliengrima <gogrinformatique@gmail.com>
2016-10-14 23:04:06 +0200

    pages atelier

--- atelier.php
+++ atelier.php
@@ -5,11 +5,11 @@
 		<div class="row">
 			<div class="col s12 m12 l12">
 				<div class="carousel carousel-slider">
-				    <a class="carousel-item" href="#one!"><img src="http://lorempixel.com/800/400/food/1"></a>
-				    <a class="carousel-item" href="#two!"><img src="http://lorempixel.com/800/400/food/2"></a>
-				    <a class="carousel-item" href="#three!"><img src="http://lorempixel.com/800/400/food/3"></a>
-				    <a class="carousel-item" href="#four!"><img src="http://lorempixel.com/800/400/food/4"></a>
-				    <a class="carousel-item" href="#five!"><img src="http://lorempixel.com/250/250/nature/5"></a>
+				    <a class="carousel-item" href="#one!"><img class="img-size" src="http://lorempixel.com/800/400/food/1"></a>
+				    <a class="carousel-item" href="#two!"><img class="img-size" src="http://lorempixel.com/800/400/food/2"></a>
+				    <a class="carousel-item" href="#three!"><img class="img-size" src="http://lorempixel.com/800/400/food/3"></a>
+				    <a class="carousel-item" href="#four!"><img class="img-size" src="http://lorempixel.com/800/400/food/4"></a>
+				    <a class="carousel-item" href="#five!"><img class="img-size" src="http://lorempixel.com/250/250/nature/5"></a>
 				</div>
 			</div>
 		</div>
