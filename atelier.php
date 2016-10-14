246bec82812c54d45378298f9b5175aad3423c4d
juliengrima <gogrinformatique@gmail.com>
2016-10-14 22:48:19 +0200

    pages atelier

--- atelier.php
+++ atelier.php
@@ -1,27 +1,19 @@
 <?php include('includes/header.php'); ?>
 
 
-	<!-- <div class="container">
+	<div class="container">
 		<div class="row">
 			<div class="col s12 m12 l12">
 				<div class="carousel carousel-slider">
-				    <a class="carousel-item" href="#one!"><img src="images/atelier1.jpg"></a>
-				    <a class="carousel-item" href="#two!"><img src="images/atelier2.jpg"></a>
-				    <a class="carousel-item" href="#three!"><img src="images/atelier3.jpg"></a>
-				    <a class="carousel-item" href="#four!"><img src="images/atelier4.jpg"></a>
-				    <a class="carousel-item" href="#five!"><img src="images/atelier5.jpg"></a>
+				    <a class="carousel-item" href="#one!"><img src="http://lorempixel.com/800/400/food/1"></a>
+				    <a class="carousel-item" href="#two!"><img src="http://lorempixel.com/800/400/food/2"></a>
+				    <a class="carousel-item" href="#three!"><img src="http://lorempixel.com/800/400/food/3"></a>
+				    <a class="carousel-item" href="#four!"><img src="http://lorempixel.com/800/400/food/4"></a>
+				    <a class="carousel-item" href="#five!"><img src="http://lorempixel.com/250/250/nature/5"></a>
 				</div>
 			</div>
 		</div>
-	</div> -->
-
-				<div class="carousel carousel-slider">
-				    <a class="carousel-item" href="#one!"><img src="images/atelier1.jpg"></a>
-				    <a class="carousel-item" href="#two!"><img src="images/atelier2.jpg"></a>
-				    <a class="carousel-item" href="#three!"><img src="images/atelier3.jpg"></a>
-				    <a class="carousel-item" href="#four!"><img src="images/atelier4.jpg"></a>
-				    <a class="carousel-item" href="#five!"><img src="images/atelier5.jpg"></a>
-				</div>
+	</div>
 
   <!-- FIN DU CORPS DE PAGE -->
 
