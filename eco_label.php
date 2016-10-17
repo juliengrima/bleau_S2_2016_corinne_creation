f9a00759b57bfffbb6fdf0d4bd2e2d6d2fc5bd48
jazz77 <pascal.jego@free.fr>
2016-10-17 11:16:52 +0200

    correction couleur police en ambert-text index.php parcours.php eco_label.php

--- eco_label.php
+++ eco_label.php
@@ -1,30 +1,30 @@
 <?php include('includes/header.php'); ?>
 
 
-	<div class="container">
-		<div class="row">
-			<div class="col s12 m6 l6">
-				<div class="vertical">
-						<span >
-							<h4>Eco-label</h4>
-							<p>
-								Dans le cadre de leur plan Climat Energie<br> 
-								Territorial (PCTE), l'agglomération Montargeoise,<br>
-								rive du loing et le syndycat mixte du pays gatinais,<br> 
-								ont organisé avec la Chambre des Métiers et de <br>
-								l'Artisanat du Loiret, l'opération "Eco-défis es commerçants<br>
-								et artisants", qui consiste à valoriser, par l'obtention d'un<br>
-								label, les artisants qui mettent en place des actions concrètes <br>
-								en faveur de l'environnement et de la gestion durable des ressources.
-							</p>
-						</span>
-				</div>
-			</div>
-			<div class="col s12 m6 l6">
-				<img src="images/ecodefis.jpg" alt="EcoLabel" class="img-size materialboxed">
-			</div>
-		</div>		
-	</div>
+<div class="container">
+    <div class="row">
+        <div class="col s12 m6 l6">
+            <div class="vertical amber-text">
+                <span >
+                    <h4>Eco-label</h4>
+                    <p>
+                        Dans le cadre de leur plan Climat Energie<br> 
+                        Territorial (PCTE), l'agglomération Montargeoise,<br>
+                        rive du loing et le syndycat mixte du pays gatinais,<br> 
+                        ont organisé avec la Chambre des Métiers et de <br>
+                        l'Artisanat du Loiret, l'opération "Eco-défis es commerçants<br>
+                        et artisants", qui consiste à valoriser, par l'obtention d'un<br>
+                        label, les artisants qui mettent en place des actions concrètes <br>
+                        en faveur de l'environnement et de la gestion durable des ressources.
+                    </p>
+                </span>
+            </div>
+        </div>
+        <div class="col s12 m6 l6">
+            <img src="images/ecodefis.jpg" alt="EcoLabel" class="img-size materialboxed">
+        </div>
+    </div>		
+</div>
 
 
 
@@ -47,6 +47,6 @@
 
 
 
-  <!-- FIN DU CORPS DE PAGE -->
+<!-- FIN DU CORPS DE PAGE -->
 
 <?php include('includes/footer.php'); ?>
