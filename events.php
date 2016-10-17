6f4ee17804caa4b8b92a1904b624391a16134c9c
juliengrima <gogrinformatique@gmail.com>
2016-10-17 10:55:54 +0200

    modif events

--- events.php
+++ events.php
@@ -4,20 +4,7 @@ $googleKey = ' AIzaSyDeoLfCFyeAzPQ-pkNyeen0aBrSovpmMFs ';
 
 ?>
 
-<div class="container">
-	<div class="row">
-		<div style='overflow:hidden;height:440px;width:700px;'>
-			<div id='gmap_canvas' style='height:440px;width:700px;'></div>
-			<div>
-				<small><a href="http://embedgooglemaps.com">google maps carte</a></small>
-			</div>
-			<div>
-				<small><a href="https://youtubeembedcode.com">youtubeembedcode.com</a></small>
-			</div>
-			<style>#gmap_canvas img{max-width:none!important;background:none!important}</style>
-		</div>
-	</div>
-</div>
+
 
 
   <!-- FIN DU CORPS DE PAGE -->
--- includes/footer.php
+++ includes/footer.php
@@ -17,9 +17,6 @@
 <script src="js/slider.js" type="text/javascript"></script>
 <script type="text/javascript" src="js/index.js"></script>
 
-<!-- GOOGLE MAPS -->
-<script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script>
-
 <!-- RETOUR VERS LE HAUT -->
 <script type="text/javascript">
 $('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
@@ -55,37 +52,9 @@ $('a.back-to-top, a.simple-back-to-top').click(function() {
         
 </SCRIPT>
 
-<!-- GOOGLE MAPS SCRIPT -->
-
-<script type='text/javascript'>
-		function init_map(){
-			// DEBUT FONCTION
-			var myOptions = {
-				zoom:9,center:new google.maps.LatLng(48.109367,2.7571100000000115),
-				mapTypeId: google.maps.MapTypeId.ROADMAP};
-
-				map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
-				marker = new google.maps.Marker({
-					map: map,position: new google.maps.LatLng(48.109367,2.7571100000000115)});
-
-				infowindow = new google.maps.InfoWindow({
-					content:'<strong>les 22 et 23 octobre au salon d\'Art et Artisanat de Nargis (45)</strong><br>nargis<br>'});
-					
-				marker = new google.maps.Marker({
-					map: map,position: new google.maps.LatLng(48.1951399,3.2688646)});
-
-				infowindow = new google.maps.InfoWindow({
-					content:'<strong>du 1er au 31 octobre, à l\'office de tourisme de Sens (89)<br>'});
 
 
-				google.maps.event.addListener(marker, 'click', 
-					function(){
-						infowindow.open(map,marker);});
-						infowindow.open(map,marker);}
-					// FIN FONCTION
 
-						google.maps.event.addDomListener(window, 'load', init_map);
-	</script>
     
     
 </body>
