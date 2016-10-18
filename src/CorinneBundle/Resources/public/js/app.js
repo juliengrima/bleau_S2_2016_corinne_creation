a1b2607c295767848bf71d3b39b2a328a072057d
brmat <baur.mathieu@laposte.net>
2016-10-18 14:45:32 +0200

    index sous symfony debut

--- src/CorinneBundle/Resources/public/js/app.js
+++ src/CorinneBundle/Resources/public/js/app.js
@@ -31,4 +31,30 @@ $(function () {
 
     $("#body-contact").hide().show(1000).css("display", "flex");
     $('#textarea1').trigger('autoresize');
+
+    // RETOUR VERS LE HAUT 
+    
+        $('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
+
+    var amountScrolled = 300;
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('a.back-to-top').fadeIn('slow');
+        } else {
+            $('a.back-to-top').fadeOut('slow');
+        }
+    });
+
+    $('a.back-to-top, a.simple-back-to-top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 700);
+        return false;
+    });
+
+    // ZOOM SUR LES IMAGES
+    
+    $('.materialboxed').materialbox();
+      
 });
