5c18d051256c451541b45825c29f2a5dcfc861ae
juliengrima <gogrinformatique@gmail.com>
2016-10-26 10:12:09 +0200

    appel presse

--- src/CorinneBundle/Controller/DefaultController.php
+++ src/CorinneBundle/Controller/DefaultController.php
@@ -65,11 +65,16 @@ class DefaultController extends Controller
     public function eventAction()
     {
         $em = $this->getDoctrine()->getManager();
-        $presses = $em->getRepository('CorinneBundle:Presse')->findAll();
+//        $presses = $em->getRepository('CorinneBundle:Presse')->findBy(event_id);
         $events = $em->getRepository('CorinneBundle:Event')->findAll();
 
+        foreach ($events as $event) {
+
+            $presses = $em->getRepository('CorinneBundle:Presse')->findBy($event.array('presse_id'->id));
+            $event['source'] = $presses['source'];
+        }
+
         return $this->render('@Corinne/User/event.html.twig', array(
-            'presses' => $presses,
             'events' => $events
         ));
     }
