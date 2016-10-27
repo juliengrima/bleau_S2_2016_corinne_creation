4b058b94f25aa52e8eed11c97bde8cdf9c13effb
jazz77 <pascal.jego@free.fr>
2016-10-27 10:04:20 +0200

    ajout du menu backend

--- src/CorinneBundle/Controller/AdminController.php
+++ src/CorinneBundle/Controller/AdminController.php
@@ -1,12 +1,9 @@
 <?php
 /**
- * Created by PhpStorm.
  * User: pascal
  * Date: 27/10/16
  * Time: 09:32
  */
-
-<?php
 
 namespace CorinneBundle\Controller;
 
@@ -14,124 +11,20 @@ use CorinneBundle\CorinneBundle;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-use CorinneBundle\Entity\Categorie;
-use CorinneBundle\Form\CategorieType;
-
 /**
- * Categorie controller.
+ * Admin controller.
  *
  */
-class CategorieController extends Controller
+class AdminController extends Controller
 {
     /**
-     * Lists all Categorie entities.
+     *
+     * main page backend admin
      *
      */
     public function indexAction()
     {
-        $em = $this->getDoctrine()->getManager();
-
-        $categories = $em->getRepository('CorinneBundle:Categorie')->findAll();
-
-        return $this->render('@Corinne/admin/categorie/index.html.twig', array(
-            'categories' => $categories,
-        ));
+        return $this->render('@Corinne/admin/index.html.twig');
     }
 
-    /**
-     * Creates a new Categorie entity.
-     *
-     */
-    public function newAction(Request $request)
-    {
-        $categorie = new Categorie();
-        $form = $this->createForm('CorinneBundle\Form\CategorieType', $categorie);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($categorie);
-            $em->flush();
-
-            return $this->redirectToRoute('categorie_show', array('id' => $categorie->getId()));
-        }
-
-        return $this->render('@Corinne/admin/categorie/new.html.twig', array(
-            'categorie' => $categorie,
-            'form' => $form->createView(),
-        ));
-    }
-
-    /**
-     * Finds and displays a Categorie entity.
-     *
-     */
-    public function showAction(Categorie $categorie)
-    {
-        $deleteForm = $this->createDeleteForm($categorie);
-
-        return $this->render('@Corinne/admin/categorie/show.html.twig', array(
-            'categorie' => $categorie,
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Displays a form to edit an existing Categorie entity.
-     *
-     */
-    public function editAction(Request $request, Categorie $categorie)
-    {
-        $deleteForm = $this->createDeleteForm($categorie);
-        $editForm = $this->createForm('CorinneBundle\Form\CategorieType', $categorie);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($categorie);
-            $em->flush();
-
-            return $this->redirectToRoute('categorie_edit', array('id' => $categorie->getId()));
-        }
-
-        return $this->render('@Corinne/admin/categorie/edit.html.twig', array(
-            'categorie' => $categorie,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Deletes a Categorie entity.
-     *
-     */
-    public function deleteAction(Request $request, Categorie $categorie)
-    {
-        $form = $this->createDeleteForm($categorie);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($categorie);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('categorie_index');
-    }
-
-    /**
-     * Creates a form to delete a Categorie entity.
-     *
-     * @param Categorie $categorie The Categorie entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Categorie $categorie)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('categorie_delete', array('id' => $categorie->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-            ;
-    }
 }
