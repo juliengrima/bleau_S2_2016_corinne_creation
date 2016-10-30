d9dc09e1ec59362fdc5a8ef82b4c60dd58024ecd
juliengrima <gogrinformatique@gmail.com>
2016-10-30 13:25:20 +0100

    probleme update

--- src/CorinneBundle/Controller/ContactController.php
+++ src/CorinneBundle/Controller/ContactController.php
@@ -101,14 +101,13 @@ class ContactController extends Controller
             $this->get('mailer')->send($message);
             $this->get('mailer')->send($message2);
 
-            return $this->redirectToRoute($routeName);
 
-//            return $this->render('@Corinne/admin/contact/new.html.twig', array(
-//                'nom' => $contact,
-//                'prenom' => $contact,
-//                'tel' => $contact,
-//                'mail' => $contact,
-//            ));
+            return $this->render('@Corinne/admin/contact/new.html.twig', array(
+                'nom' => $contact,
+                'prenom' => $contact,
+                'tel' => $contact,
+                'mail' => $contact,
+            ));
         }
         else {
 //            ENVOI DU MAIL
